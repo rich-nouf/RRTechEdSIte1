fdcd54642269180e8beb371416b27cc4ed16a176
R Renouf <rich_nouf@yahoo.com>
2014-11-01 22:30:41 +1300

    Add Application Insights instrumentation

---
+++ RRTechEdSite1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RRTechEdSite1.MvcApplication" Language="C#" %>
---
+++ RRTechEdSite1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RRTechEdSite1.Startup))]
+namespace RRTechEdSite1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
